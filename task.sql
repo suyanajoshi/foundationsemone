d1a80e68174c43a80283ba736dbfa6893c081784
suyanajoshi <joshisuyana5@gmail.com>
2026-02-27 23:29:48 +0545

    created and inserted values in the table

--- task.sql
+++ task.sql
@@ -1,1 +1,21 @@
-
+CREATE TABLE StudentClubs (
+    StudentID INT,
+    StudentName VARCHAR(50),
+    Email VARCHAR(100),
+    ClubName VARCHAR(50),
+    ClubRoom VARCHAR(20),
+    ClubMentor VARCHAR(50),
+    JoinDate DATE
+);
+INSERT INTO StudentClubs (StudentID, StudentName, Email, ClubName, ClubRoom, ClubMentor, JoinDate)
+VALUES 
+(1, 'Asha', 'asha@email.com', 'Music Club', 'R101', 'Mr. Raman', '2024/01/10'),
+(2, 'Bikash', 'bikash@email.com', 'Sports Club', 'R202', 'Ms. Sita', '2024/01/12'),
+(1, 'Asha', 'asha@email.com', 'Sports Club', 'R202', 'Ms. Sita', '2024/01/15'),
+(3, 'Nisha', 'nisha@email.com', 'Music Club', 'R101', 'Mr. Raman', '2024/01/20'),
+(4, 'Rohan', 'rohan@email.com', 'Drama Club', 'R303', 'Mr. Kiran', '2024/01/18'),
+(5, 'Suman', 'suman@email.com', 'Music Club', 'R101', 'Mr. Raman', '2024/01/22'),
+(2, 'Bikash', 'bikash@email.com', 'Drama Club', 'R303', 'Mr. Kiran', '2024/01/25'),
+(6, 'Pooja', 'pooja@email.com', 'Sports Club', 'R202', 'Ms. Sita', '2024/01/27'),
+(3, 'Nisha', 'nisha@email.com', 'Coding Club', 'Lab1', 'Mr. Anil', '2024/01/28'),
+(7, 'Aman', 'aman@email.com', 'Coding Club', 'Lab1', 'Mr. Anil', '2024/01/30');
